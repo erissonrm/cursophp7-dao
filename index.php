22270d02655669f0b4348c6ff051d1088bd39cc4
Erisson <erisson.erm@gmail.com>
2018-02-08 16:31:28 -0300

    classe usuario criada

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
 require_once("config.php");
 
+$root = new Usuario();
+
+$root->loadById(4);
+
+echo $root;
+/*
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
-
+*/
 ?>
