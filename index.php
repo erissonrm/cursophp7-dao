6b6745ea179e1a93233374c0ea7fbe03b99df5d0
Erisson <erisson.erm@gmail.com>
2018-02-09 12:43:34 -0300

    metodos de lista do usuario

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
 require_once("config.php");
 
+/*carrega um usuario
 $root = new Usuario();
-
 $root->loadById(4);
+echo $root;*/
 
-echo $root;
+//LISTA TODOS OS USUARIOS
+/*
+$lista = usuario::getList();
+echo json_encode($lista);*/
+
+//CARREGA LISTA DE USUARIOS POR LOGIN
+/*$search = Usuario::search("pe");
+echo json_encode($search);*/
+
+//carrega usuario usando login e senha
+$usuario = new Usuario();
+$usuario->login("Pedro","qwe123");
+echo $usuario;
+
 /*
 $sql = new Sql();
 
